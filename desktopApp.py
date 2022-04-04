e016bde253481c031f0dd7c5911ffaf8093f4b77
sebastiangrosvik <100279796+sebastiangrosvik@users.noreply.github.com>
2022-04-04 21:56:59 +0200

    v1

--- desktopApp.py
+++ desktopApp.py
@@ -3,33 +3,44 @@ from mqtt import MQTT_Client_1
 from threading import Thread
 import paho.mqtt.client as mqtt
 
-#Define root window
-root = Tk()
-root.title('Coffee talk - Desktop application')
-root.geometry("400x100")
-
-#Valid users:
-names = ["Emil", "Emilie", "Hanne", "Jonathan", "Sebastian"]
 
 #Login function
 def login():
+    #Valid users:
+    names = ["Emil", "Emilie", "Hanne", "Jonathan", "Sebastian"]
+    global wrong_label
+    wrong_label = Label(root, text="Wrong login")
     if myTextbox.get() in names:
         loggedin()
         mqtt()
     else:
-        root.destroy()
+        wrong_label.pack()
+        root.after(1000, lambda: wrong_label.destroy())
+        root.after(1000)
+        
 
+
+#Signout function
 def signout():
     root.destroy()
+    idle()
 
+#Refresh function
+def refresh():
+    print("refresh")
+
+#Logged in state
 def loggedin():
     hello_label = Label(root, text="Welcome " + myTextbox.get())
+    refreshButton = Button(root, text="Refresh", command = refresh)
     quitButton = Button(root, text="Sign out", command = signout)
     myLabel.destroy()
     myTextbox.destroy()
     myButton.destroy()
     hello_label.pack()
+    refreshButton.pack()
     quitButton.pack()
+    
 
 #MQTT function
 def mqtt():
@@ -38,14 +49,22 @@ def mqtt():
     #client = MQTT_Client_1()
     #client.start(broker, port)
 
+#Idle state
+def idle():
+    global root, myLabel, myButton, myTextbox
+    #Define root window
+    root = Tk()
+    root.title('Coffee talk - Desktop application')
+    root.geometry("400x100")
+    #Root window features
+    myLabel = Label(root, text="Enter name: ")
+    myLabel.pack()
+    myTextbox = Entry(root, width=30)
+    myTextbox.pack()
+    myButton = Button(root, text="Login", command= login)
+    myButton.pack()
 
-#Root window features
-myLabel = Label(root, text="Enter name: ")
-myLabel.pack()
-myTextbox = Entry(root, width=30)
-myTextbox.pack()
-myButton = Button(root, text="Login", command= login)
-myButton.pack()
+    #Main loop for tkinter
+    root.mainloop()
 
-#Main loop for tkinter
-root.mainloop()
+idle()
