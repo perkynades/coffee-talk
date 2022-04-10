e4d1f9ce577bde2bd7582801f594e8e19deb7dc2
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-04-10 18:45:14 +0200

    Ok now the ui is mostly finished

--- web/src/components/Room.tsx
+++ web/src/components/Room.tsx
@@ -2,17 +2,32 @@ import { Button, Modal } from 'antd'
 import React, { ReactElement, useState } from 'react'
 import './Room.css'
 
-export default function Room({roomName}: {roomName: string}): ReactElement {
+export default function Room({
+    roomName,
+    peopleInRoom
+}: {
+    roomName: string
+    peopleInRoom: string[]
+}): ReactElement {
     const [modalVisible, setModalVisible] = useState<boolean>(false)
-    
+
     const onModalOk = () => {
-        
+        // Add logic for starting python stuff
+    }
+
+    const createPeopleInRoomElement = (): ReactElement => {
+        return peopleInRoom.length === 0 ? <div className='people-in-room'><p>Room is empty</p></div> : (
+            <div className='people-in-room'>
+                {peopleInRoom.map((value, i) => <p key={i}>{value}</p>)}
+            </div>
+        );
     }
 
     return (
         <div className='room-container'>
             <h2>{roomName}</h2>
             <div>
+                {createPeopleInRoomElement()}
                 <Button type='primary' onClick={() => setModalVisible(true)}>
                     Join {roomName} room
                 </Button>
--- web/src/views/office/Office.tsx
+++ web/src/views/office/Office.tsx
@@ -3,17 +3,27 @@ import { useLocation } from 'react-router-dom'
 import Room from '../../components/Room'
 import './Office.css'
 
+interface IPeopleInRoom {
+    peopleInRoom: string[]
+}
+
 export default function Office(): ReactElement {
     const { state }: {state: any} = useLocation()
+    
+    const [peopleInRooms, setPeopleInRooms] = useState<IPeopleInRoom>()
+
+    const populatePeopleInRooms = () => {
+        // Add communication with mqtt broker?
+    }
 
     return (
         <div>
             <h1 className='office-info'>Sales AS, Oslovegen 1</h1>
             <div className='rooms-container'>
-                <Room roomName='Cantine' />
-                <Room roomName='Coffee' />
-                <Room roomName='Ping-pong' />
-                <Room roomName='Lounge' />
+                <Room roomName='Cantine' peopleInRoom={["Emil", "Sebastian"]}/>
+                <Room roomName='Coffee' peopleInRoom={["Hanne"]}/>
+                <Room roomName='Ping-pong' peopleInRoom={["Jonatan", "Emilie"]}/>
+                <Room roomName='Lounge' peopleInRoom={[]}/>
             </div>
         </div>
     )
