85b907d7ec43ca0a96fcfc444dea37ff3b0bfea4
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-04-10 18:16:35 +0200

    Finish gui things almost

--- web/src/components/Room.tsx
+++ web/src/components/Room.tsx
@@ -1,10 +1,30 @@
-import React, { ReactElement } from 'react'
+import { Button, Modal } from 'antd'
+import React, { ReactElement, useState } from 'react'
 import './Room.css'
 
 export default function Room({roomName}: {roomName: string}): ReactElement {
+    const [modalVisible, setModalVisible] = useState<boolean>(false)
+    
+    const onModalOk = () => {
+        
+    }
+
     return (
         <div className='room-container'>
             <h2>{roomName}</h2>
+            <div>
+                <Button type='primary' onClick={() => setModalVisible(true)}>
+                    Join {roomName} room
+                </Button>
+                <Modal
+                    title={"Join " + roomName + "?"} 
+                    visible={modalVisible}
+                    onOk={onModalOk}
+                    onCancel={() => setModalVisible(false)}
+                >
+                    <p>Do you want to join the {roomName.toLowerCase()} room?</p>
+                </Modal>
+            </div>
         </div>
     )
 }
